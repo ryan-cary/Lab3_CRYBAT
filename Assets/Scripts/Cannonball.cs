8b787990b5b189dda56abfafb1666d83f31efb54
scion <60977346+ryan-cary@users.noreply.github.com>
2026-07-29 20:38:31 -0700

    Update Cannonball.cs
    Automatically destroys itself after specified lifetime

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -5,6 +5,11 @@ public class Cannonball : MonoBehaviour
     [SerializeField] float _damage;
     [SerializeField] float _lifetime = 5;
 
+    void Start()
+    {
+        Destroy(gameObject, _lifetime);
+    }
+
     void OnCollisionEnter2D (Collision2D collision)
     {
         //when the ball collides with something, deal damage to it!
